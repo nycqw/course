cb2d85e5d9ff2bc31357c3e8ae8ab26c13cd3f0a
nycqw <295838198@qq.com>
2018-09-11 20:30:40 +0800

    日志

--- src/main/java/com/eden/service/DataService.java
+++ src/main/java/com/eden/service/DataService.java
@@ -2,6 +2,7 @@ package com.eden.service;
 
 import com.eden.mapper.EpayOrderMapper;
 import com.eden.model.EpayOrder;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,12 +10,14 @@ import org.springframework.stereotype.Service;
  * Created by 18060757 on 2018/9/11.
  */
 @Service
+@Slf4j
 public class DataService {
 
     @Autowired
     private EpayOrderMapper epayOrderMapper;
 
     public EpayOrder queryOrder(Long orderId) {
+        log.info(orderId.toString());
         return epayOrderMapper.selectByPrimaryKey(orderId);
     }
 }
